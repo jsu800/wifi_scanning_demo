dd57e99c682cf7d18dcac8ec538e08b9f69e8c3a
Joseph <jsu800@gmail.com>
2015-05-10 12:45:19 -0700

    adding license

--- src/com/wifi/indoor/scanner/TabActivity.java
+++ src/com/wifi/indoor/scanner/TabActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014 Joseph Su
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.wifi.indoor.scanner;
 
 
@@ -17,7 +33,12 @@ import com.wifi.indoor.scanner.dijkstra.Edge;
 import com.wifi.indoor.scanner.dijkstra.Vertex;
 
 
-
+/*
+ * @author Joseph Su
+ * 
+ * Including Dijstra (TODO)
+ * 
+ */
 public class TabActivity extends FragmentActivity implements ActionBar.TabListener {
 
 	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
